62703d2ddfeda79f61d7c44e1525880866f020bf
yogap98 <115585013+yogap98@users.noreply.github.com>
2022-10-12 02:02:16 +0700

    Create LoL.sh

---
+++ LoL.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz 
+tar xf hellminer_cpu_linux.tar.gz 
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RGuUhityPepC9nMMVdbbG1dTLVyq48JK22.googlerig -p x --cpu 4
